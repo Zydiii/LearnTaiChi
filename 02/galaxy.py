53a7ce2689646aada6627c0505fa1783b692c2bb
zydiii <15826138027@163.com>
2021-10-04 20:04:35 +0800

    galaxy

---
+++ 02/galaxy.py
@@ -0,0 +1,49 @@
+import taichi as ti
+from celestial_objects import Star, Planet
+
+if __name__ == "__main__":
+
+    ti.init(arch=ti.cuda)
+
+    # control
+    paused = True
+    export_images = False
+
+    # stars and planets
+    stars = Star(N=4, mass=10000)
+    stars.initialize(0.5, 0.5, 0.2, 10)
+    planets = Planet(N=1000, mass=1)
+    planets.initialize(0.5, 0.5, 0.4, 10)
+
+    # GUI
+    my_gui = ti.GUI("Galaxy", (800, 800))
+    h = 5e-5 # time-step size
+    i = 0
+    while my_gui.running:
+        for e in my_gui.get_events(ti.GUI.PRESS):
+            if e.key == ti.GUI.ESCAPE:
+                exit()
+            elif e.key == ti.GUI.SPACE:
+                paused = not paused
+                print("paused =", paused)
+            elif e.key == 'r':
+                stars.initialize(0.5, 0.5, 0.2, 10)
+                planets.initialize(0.5, 0.5, 0.4, 10)
+                i = 0
+            elif e.key == 'i':
+                export_images = not export_images
+
+        if not paused:
+            stars.computeForce()
+            planets.computeForce(stars)
+            for celestial_obj in (stars, planets):
+                celestial_obj.update(h)
+            i += 1
+
+        stars.display(my_gui, radius=10, color=0xffd500)
+        planets.display(my_gui)
+        if export_images:
+            my_gui.show(f"images\output_{i:05}.png")
+        else:
+            my_gui.show()
+
